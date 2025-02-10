b5c98e869ffadcebe447b51b446af9f4db527f99
l-efa <lefa.sahi@gmail.com>
2025-02-10 11:05:14 +0200

    github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-1-tehtava-anekdootit/",
+  base: "/react-1-tehtava-anekdootit-/",
   plugins: [react()],
   server: {
     host: true,
